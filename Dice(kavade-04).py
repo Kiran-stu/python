218bbadde9dc667383dd46c67b43eaf0e3e3a96c
Kiran V <61462931+Kiran-stu@users.noreply.github.com>
2021-02-02 22:01:59 +0530

    Dice
    Dice for the game chowkabara 5x5. People who know the game can get it.

---
+++ Dice(kavade-04).py
@@ -0,0 +1,16 @@
+import random
+
+t=(2,4,8,1,3)
+i=random.randint(0,4)
+
+restart=0
+print('ಕವಡೆ-04\n')
+print('Press enter to start ...\n'+'To exit press x')
+
+while restart!='x':
+    restart=input()
+    i = random.randint(0, 4)
+    print(t[i])
+    
+    
+#people who know chowka bara game(5x5 boxes) here is your dice
